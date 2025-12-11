4809f4921a3a33ad19d2758af433792d3cdc92a4
saavedra75 <josemanuel75saavedra@gmail.com>
2025-12-11 21:02:22 +0100

    guardo el index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,33 @@
+
+
+export interface Character {
+    id:       number;
+    name:     string;
+    status:   Status;
+    species:  Species;
+    type:     string;
+    gender:   string;
+    origin:   Location;
+    location: Location;
+    image:    string;
+    episode:  string[];
+    url:      string;
+    created:  Date;
+}
+
+
+export interface Location {
+    name: string;
+    url:  string;
+}
+
+export enum Species {
+    Alien = "Alien",
+    Human = "Human",
+}
+
+export enum Status {
+    Alive = "Alive",
+    Dead = "Dead",
+    Unknown = "unknown",
+}
