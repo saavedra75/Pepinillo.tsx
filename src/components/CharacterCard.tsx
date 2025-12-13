2650e381d75c327de150f498ece12d3cfbe471a3
FJMG1 <fmolinagarcia94@gmail.com>
2025-12-13 11:23:13 +0100

    Botón de buscador funcional, falta la conexión ShipContexts para el agregado del botón contratar. Estilo aun por definir del todo.

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,42 +1,28 @@
-import { useEffect, useState } from "react";
 import type { Character } from "../types";
 import "./CharacterCard.css"
 
-export default function CharacterCard(){
-const[crews, setCrews]=useState<Character[]>([]); //Estado para almacenar los posts
+interface Props{
+  crew: Character;
+  onHire: (id:number)=>void;
+}
 
-  useEffect(()=>{
-    //Realizo la petición a la API
-    fetch("https://rickandmortyapi.com/api/character")
-    .then(response=>response.json())
-    .then(data=>{
-      setCrews(data.results); //Guardo los dados en el estado
-    })
-    .catch(error=>{
-      console.error("Error fetching posts:",error);
-    });
-  },[]); //[] asegura que solo se ejecute una vez(al montar el componente)
-
-   function addCrew(id){
-
-   }
+export default function CharacterCard({crew, onHire}:Props){  
   return(
-    <>
-         {crews.map(crew=>(
-         <div className="crew-card"key={crew.id}>
-         <img src={crew.image} alt={crew.name}/>
-         <h3>{crew.name}</h3>
-         <p>{crew.species}</p>
-         <small>{crew.status}</small>
-        <div className="card-action">
-          <button
-          className="btn-action btn-feed" 
-          onClick={()=>addCrew(crew.id)}
+    <>        
+    <div className="crew-card"key={crew.id}>
+      <img src={crew.image} alt={crew.name}/>
+      <h3>{crew.name}</h3>
+      <p>{crew.species}</p>
+      <small>{crew.status}</small>
+      <div className="card-action">
+        <button
+        className="btn-action btn-feed" 
+        onClick={()=>onHire(crew.id)}
           disabled={crew.status==="Dead"||crew.status==="unknown"}
           >Contratar</button>
         </div>                         
-         </div>
-            ))}
+    </div>            
     </>
         )
-    }
+        }
+    
--- src/pages/HireCrew.tsx
+++ src/pages/HireCrew.tsx
@@ -1,17 +1,47 @@
+import { useEffect, useState } from "react";
 import CharacterCards from "../components/CharacterCard";
+import type { Character } from "../types";
 import "./HireCrew.css"
 
-
 export default function HireCrew(){
-  
+
+const[crews, setCrews]=useState<Character[]>([]); //Estado para almacenar los posts
+const [searchCrew, setSearchCrew]=useState(""); //Estado para las busquedas
+
+  useEffect(()=>{
+    //Realizo la petición a la API
+    fetch("https://rickandmortyapi.com/api/character")
+    .then(response=>response.json())
+    .then(data=>{
+      setCrews(data.results); //Guardo los dados en el estado
+    })
+    .catch(error=>{
+      console.error("Error fetching posts:",error);
+    });
+  },[]); //[] asegura que solo se ejecute una vez(al montar el componente)
+
+
+  const filteredCrews = crews.filter((crews)=>
+  crews.name.toLowerCase().includes(searchCrew.toLocaleLowerCase())
+  );
+
+  function addCrew(id: number){
+    //Conexión de Shipcontexts
+
+  }
     return (
         <>
           <h2>HireCrew</h2>
-                    <input type="text"  placeholder="Buscar por nombre..."/>
-          <button>Buscar</button>
-       
+          <input 
+          type="text"  
+          placeholder="Buscar tripulante..."
+          value={searchCrew}
+          onChange={(e)=>setSearchCrew(e.target.value)}
+          />
           <div className="crew-grid">
-          <CharacterCards/>
+          {filteredCrews.map((crew)=>(
+            <CharacterCards key={crew.id} crew={crew} onHire={addCrew}/>
+          ))}
          
         </div>
       </>
