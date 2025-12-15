55763517c13144b93a1beb43d982c0d4cbaa87af
saavedra75 <josemanuel75saavedra@gmail.com>
2025-12-15 21:15:36 +0100

    empiezo la función onSubmit

--- src/pages/Missions.tsx
+++ src/pages/Missions.tsx
@@ -2,25 +2,36 @@ import { ShipContext } from "../context/ShipContext";
 import { useShip } from "../hooks/useShip";
 
 
+//Función que genera un resultado aleatorio para la misión.
+function generateResult() {
+  return Math.random() === 1 ? 'Victory' : 'Defeat';
+}
+
+
 
 export default function Missions(){
 
-/**
- * Requisitos:
- * - Acceder al contexto de la nave (crew, credits y fuel)
- * - Acceder a los planetas de la API
- * - 
- */
-
-//Formulario para realizar una misión 
-
+//Importo las funciones y estados que necesitaré para las misiones
 const {credits, crew, fuel, reduceFuel, spendCredits} = useShip();
-  credits;
-  spendCredits(0);
+
+  //Función que se ejecuta al enviar el formulario de la misión. 
+  function startMission() {
+
+    //Obtengo los datos 
+    //Combustible que se va a usar (entre 15% y 40%)
+    let wastedFuel: number = Math.floor(Math.random() * (40 - 15 + 1)) + 15;
+
+    //Si falta combustible la misión será cancelada, si no será victoria o derrota
+    if (wastedFuel > fuel) {
+      let result: string = 'Cancelled';
+    } else {
+      let result: string = generateResult();
+    }
+  }
 
 
   return (
-    <form>
+    <form onSubmit={startMission}>
       <label htmlFor="crew"></label>
         <select name="crew" id="crew">
         {crew.map(crewMate => (
@@ -30,6 +41,8 @@ const {credits, crew, fuel, reduceFuel, spendCredits} = useShip();
       <label htmlFor="planet"></label>
         <select name="planet" id="planet">
         {/* Aquí mapearé los planetas */}
+        <option value={"Earth"}>Earth</option>
+        <option value={"Mars"}>Mars</option>
       </select>
 
       <button type="submit" disabled={fuel <= 0}>{fuel <= 0 ? 'No fuel' : 'Send'}</button>
