d9b725f9c02dbed91032d3c3813a6bf3f88520b5
saavedra75 <josemanuel75saavedra@gmail.com>
2025-12-15 10:01:41 +0100

    Subo la estructura del formulario

--- src/pages/Missions.tsx
+++ src/pages/Missions.tsx
@@ -1,8 +1,39 @@
+import { ShipContext } from "../context/ShipContext";
+import { useShip } from "../hooks/useShip";
+
+
+
 export default function Missions(){
-    return (
-        <div>
-          <h2>Missions</h2>
-        </div>
-    );
+
+/**
+ * Requisitos:
+ * - Acceder al contexto de la nave (crew, credits y fuel)
+ * - Acceder a los planetas de la API
+ * - 
+ */
+
+//Formulario para realizar una misión 
+
+const {credits, crew, fuel, reduceFuel, spendCredits} = useShip();
+  credits;
+  spendCredits(0);
+
+
+  return (
+    <form>
+      <label htmlFor="crew"></label>
+        <select name="crew" id="crew">
+        {crew.map(crewMate => (
+          <option value={crewMate.id}>{crewMate.name}</option>
+        ))}
+      </select>
+      <label htmlFor="planet"></label>
+        <select name="planet" id="planet">
+        {/* Aquí mapearé los planetas */}
+      </select>
+
+      
+    </form>
+  );
 
 }
