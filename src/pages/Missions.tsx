9381c0358d53164a0b4df3db60ce85f056fa855d
saavedra75 <josemanuel75saavedra@gmail.com>
2025-12-16 11:48:44 +0100

    arreglo el formulario para que se vean los planetas dinámicamente

--- src/pages/Missions.tsx
+++ src/pages/Missions.tsx
@@ -1,18 +1,34 @@
+import { useEffect, useState } from "react";
 import { ShipContext } from "../context/ShipContext";
 import { useShip } from "../hooks/useShip";
-
+import { getLocations } from "../services/rickAndMortyService";
+import type { ILocation } from "../types/index";
 
 //Función que genera un resultado aleatorio para la misión.
 function generateResult() {
-  return Math.random() === 1 ? 'Victory' : 'Defeat';
+  return Math.random() === 1 ? 'Success' : 'Failure';
 }
 
 
 
 export default function Missions(){
 
-//Importo las funciones y estados que necesitaré para las misiones
-const {credits, crew, fuel, reduceFuel, spendCredits} = useShip();
+  //Importo las funciones y estados que necesitaré para las misiones
+  const {addCredits, crew, fuel, reduceFuel} = useShip();
+
+
+  //Saco los panetas para usarlos en el formulario
+
+  const [planets, setPlanets] = useState<ILocation[]>([]);
+
+  //Por asincronía tengo que hacer un useEffect para que al cargar el componente espere al fetch de los planetas
+  useEffect(() => {
+    const fetchPlanets = async () => {
+      const data = await getLocations();
+      setPlanets(data.results);
+    };
+    fetchPlanets();
+  }, []);
 
   //Función que se ejecuta al enviar el formulario de la misión. 
   function startMission() {
@@ -22,12 +38,18 @@ const {credits, crew, fuel, reduceFuel, spendCredits} = useShip();
     let wastedFuel: number = Math.floor(Math.random() * (40 - 15 + 1)) + 15;
 
     //Si falta combustible la misión será cancelada, si no será victoria o derrota
+    let result: String;
     if (wastedFuel > fuel) {
-      let result: string = 'Cancelled';
+      result = 'Cancelled';
     } else {
-      let result: string = generateResult();
+      result = generateResult();
     }
 
+    if (result === 'Success') {
+      reduceFuel(wastedFuel);
+    } else if (result === 'Failure') {
+      reduceFuel(wastedFuel);
+    }
     
   }
 
@@ -37,14 +59,14 @@ const {credits, crew, fuel, reduceFuel, spendCredits} = useShip();
       <label htmlFor="crew"></label>
         <select name="crew" id="crew">
         {crew.map(crewMate => (
-          <option value={crewMate.id}>{crewMate.name}</option>
+          <option key={crewMate.id} value={crewMate.id}>{crewMate.name}</option>
         ))}
       </select>
       <label htmlFor="planet"></label>
         <select name="planet" id="planet">
-        {/* Aquí mapearé los planetas */}
-        <option value={"Earth"}>Earth</option>
-        <option value={"Mars"}>Mars</option>
+        {planets.map(planet => (
+          <option key={planet.name} value={planet.name}>{planet.name}</option>
+        ))}
       </select>
 
       <button type="submit" disabled={fuel <= 0}>{fuel <= 0 ? 'No fuel' : 'Send'}</button>
