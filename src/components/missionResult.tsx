3d79cfa755adf3e23a23a7c548438b3b3efe99cb
pepe-espino <joseantespinodz@gmail.com>
2025-12-16 17:07:21 +0100

    solucion archivo modificado

--- src/components/missionResult.tsx
+++ src/components/missionResult.tsx
@@ -1,13 +1,3 @@
-import type { IMissionSum } from "../types"
-
-export default function MissionResult() {
-
-    return (
-        <div>
-            <h2>Mission summarise:</h2>
-            <p><b>General result: {}</b></p>
-            <p><b>Earned Credits: {}</b></p>
-            <p><b>Wasted Fuel: {}</b></p>
-        </div>
-    )
+export default function MissionResult({}) {
+    
 }
